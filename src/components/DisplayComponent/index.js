ac425f4592a2378263169bc21cb4308a7ac4e36c
poppymills2@gmail.com <poppymills2@gmail.com>
2021-11-07 21:21:40 -0500

    updated hero picture

---
+++ src/components/DisplayComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Display() {
+  return (
+    <div className="main-display">
+      <h1>Display Component</h1>
+    </div>
+  );
+}
+
+export default Display;
